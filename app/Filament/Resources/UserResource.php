a336854f55c6e119c7383b8e24e6fd87f9da9172
John Puddephatt <john@jdp.org.uk>
2021-10-13 14:15:12 +0100

    default true on approved box

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -29,7 +29,7 @@ class UserResource extends Resource
                             Components\TextInput::make('email'),
                             Components\DatePicker::make('date_of_birth'),
                             // Components\FileUpload::make('photo')->image()->imageCropAspectRatio('1:1')->disk('public'),
-                            Components\Checkbox::make('approved'),
+                            Components\Checkbox::make('approved')->default(true),
                             Components\Select::make('type')->options(['member' => 'Member', 'supporter' => 'Supporter', 'organisation' => 'Organisation']),
                         ],
                     ),
