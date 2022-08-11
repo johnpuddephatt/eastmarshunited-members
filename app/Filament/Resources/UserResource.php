e8755209d160360df685753500c634ab7ee2b432
John Puddephatt <john@jdp.org.uk>
2022-08-11 12:36:14 +0100

    add dob and created_at to users table

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -16,70 +16,87 @@ class UserResource extends Resource
 {
     public $recordsPerPage = 50;
 
-    public static $icon = 'heroicon-o-user';
+    public static $icon = "heroicon-o-user";
 
     public static function form(Form $form)
     {
-        return $form
-            ->schema([
-                Components\Tabs::make('Label')
-                ->tabs([
-                    Components\Tab::make(
-                        'Account',
-                        [
-                            Components\TextInput::make('name')->required(),
-                            Components\TextInput::make('email')->required(),
-                            Components\DatePicker::make('date_of_birth')->required(),
-                            // Components\FileUpload::make('photo')->image()->imageCropAspectRatio('1:1')->disk('public'),
-                            
-                            Components\Select::make('type')->options(['member' => 'Member', 'supporter' => 'Supporter', 'organisation' => 'Organisation'])->default('member')->dependable(),
-                            Components\Checkbox::make('approved')->when(fn ($record) => $record->type === 'organisation'),
-                            Components\Textarea::make('notes'),
-                        ],
+        return $form->schema([
+            Components\Tabs::make("Label")->tabs([
+                Components\Tab::make("Account", [
+                    Components\TextInput::make("name")->required(),
+                    Components\TextInput::make("email")->required(),
+                    Components\DatePicker::make("date_of_birth")->required(),
+                    // Components\FileUpload::make('photo')->image()->imageCropAspectRatio('1:1')->disk('public'),
+
+                    Components\Select::make("type")
+                        ->options([
+                            "member" => "Member",
+                            "supporter" => "Supporter",
+                            "organisation" => "Organisation",
+                        ])
+                        ->default("member")
+                        ->dependable(),
+                    Components\Checkbox::make("approved")->when(
+                        fn($record) => $record->type === "organisation"
                     ),
-                    Components\Tab::make(
-                        'Timebanking',
-                        [
-                            Components\Checkbox::make('has_completed_profile'),
-                            Components\TextInput::make('credits'),
-                            Components\TextInput::make('phone'),
-                            Components\Textarea::make('description'),
-                        ],
-                    ),
+                    Components\Textarea::make("notes"),
                 ]),
-            ]);
+                Components\Tab::make("Timebanking", [
+                    Components\Checkbox::make("has_completed_profile"),
+                    Components\TextInput::make("credits"),
+                    Components\TextInput::make("phone"),
+                    Components\Textarea::make("description"),
+                ]),
+            ]),
+        ]);
     }
 
     public static function table(Table $table)
     {
         return $table
             ->columns([
-                Columns\Text::make('name')->primary(),
-                Columns\Text::make('email'),
-                Columns\Text::make('type'),
-                Columns\Boolean::make('has_completed_profile')->label('Timebanking')
+                Columns\Text::make("name")->primary(),
+                Columns\Text::make("email"),
+                Columns\Text::make("date_of_birth"),
+                Columns\Text::make("created_at"),
+                Columns\Text::make("type"),
+                Columns\Boolean::make("has_completed_profile")->label(
+                    "Timebanking"
+                ),
             ])
             ->filters([
-                Filter::make('Only members', fn ($query) => $query->where('type', 'member')),
-                Filter::make('Only supporters', fn ($query) => $query->where('type', 'supporter')),
-                Filter::make('Only organisations', fn ($query) => $query->where('type', 'organisation')),
-                Filter::make('Show deleted users', fn ($query) => $query->onlyTrashed())
+                Filter::make(
+                    "Only members",
+                    fn($query) => $query->where("type", "member")
+                ),
+                Filter::make(
+                    "Only supporters",
+                    fn($query) => $query->where("type", "supporter")
+                ),
+                Filter::make(
+                    "Only organisations",
+                    fn($query) => $query->where("type", "organisation")
+                ),
+                Filter::make(
+                    "Show deleted users",
+                    fn($query) => $query->onlyTrashed()
+                ),
             ]);
     }
 
     public static function relations()
     {
         return [
-            //
-        ];
+                //
+            ];
     }
 
     public static function routes()
     {
         return [
-            Pages\ListUsers::routeTo('/', 'index'),
-            Pages\CreateUser::routeTo('/create', 'create'),
-            Pages\EditUser::routeTo('/{record}/edit', 'edit'),
+            Pages\ListUsers::routeTo("/", "index"),
+            Pages\CreateUser::routeTo("/create", "create"),
+            Pages\EditUser::routeTo("/{record}/edit", "edit"),
         ];
     }
 }
